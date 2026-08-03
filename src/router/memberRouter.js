01db9a570b27b451511d01c08f344fc1a288cb66
s3618222 <s3618222@gmail.com>
2026-08-03 13:16:53 +0800

    add 新建會員中心內容區切換的路由檔

---
+++ src/router/memberRouter.js
@@ -0,0 +1,39 @@
+//會員中心使用的各區塊切換路由
+import {
+  createRouter,
+  createWebHashHistory
+} from "vue-router";
+
+import MyBattle from "@/components/member/battle/myBattle.vue";
+import MyForum from "@/components/myForum.vue";
+import MyExchange from "@/components/myExchange.vue";
+
+const memberRouter = createRouter({
+  history: createWebHashHistory(),
+
+  //後續新建區塊，需在此處增加對應路徑與名稱
+  routes: [
+    {
+      path: "/",
+      redirect: "/member/battle"
+      //當前台透過href="./member.html"，進入會員中心時，會預設導覽至我的約戰專區，後續再調整成側邊欄位第一個顯示的區塊連結是什麼
+    },
+    {
+      path: "/member/battle",
+      name: "member-battle",
+      component: MyBattle
+    },
+    {
+      path: "/member/forum",
+      name: "member-forum",
+      component: MyForum
+    },
+    {
+      path: "/member/exchange",
+      name: "member-exchange",
+      component: MyExchange
+    }
+  ]
+});
+
+export default memberRouter;
