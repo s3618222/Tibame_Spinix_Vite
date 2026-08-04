9617aafd73fb0372e60b718307e5cfb18d239fe1
cyling <chuang5517@gmail.com>
2026-08-04 16:01:53 +0800

    update market.html banner RWD

--- src/assets/js/public.js
+++ src/assets/js/public.js
@@ -1,5 +1,6 @@
 // header logo 旋轉效果
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
+const header = document.querySelector('#headerApp');
 
 function changeHeader() {
     if (window.scrollY > 40) {
