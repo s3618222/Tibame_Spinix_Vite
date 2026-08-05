cf0a4b824482eb8cb525216bf0d54f77fb5bec5c
s3618222 <s3618222@gmail.com>
2026-08-05 08:53:54 +0800

    add element分頁元件

---
+++ src/components/pagination.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-pagination
+    :current-page="currentPage"
+    :page-size="pageSize"
+    :total="total"
+    layout="prev, pager, next"
+    @current-change="changePage"
+  />
+</template>
+
+<script>
+  export default {
+    name: "Pagination",
+
+    props: {
+      currentPage: { //現在第幾頁
+        type: Number,
+        required: true
+      },
+
+      pageSize: { //每頁顯示幾筆
+        type: Number,
+        required: true
+      },
+
+      total: { //總筆數
+        type: Number,
+        required: true
+      }
+    },
+
+    emits: ["update:currentPage"],
+
+    methods: {
+      changePage(page) {
+        this.$emit("update:currentPage", page);
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+</style>
