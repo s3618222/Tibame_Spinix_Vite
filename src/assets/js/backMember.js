053f685ef0fcafceeb3e0ce89d1ee49842cb42dd
s3618222 <s3618222@gmail.com>
2026-08-05 08:55:00 +0800

    update 後台彙整用js 套用分頁元件

--- src/assets/js/backMember.js
+++ src/assets/js/backMember.js
@@ -3,4 +3,11 @@ import { createApp } from "vue";
 import BackMember from "@/views/backend/backMember.vue";
 import backMemberRouter from "@/router/backMemberRouter.js";
 
-createApp(BackMember).use(backMemberRouter).mount("#backMemberApp");
+// 導入 Element Plus
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+createApp(BackMember)
+  .use(backMemberRouter)
+  .use(ElementPlus)
+  .mount("#backMemberApp");
