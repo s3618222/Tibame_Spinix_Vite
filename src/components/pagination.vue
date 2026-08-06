476e577a244c4c6a7c1cda0b8e8ebf271952aeff
s3618222 <s3618222@gmail.com>
2026-08-06 15:16:30 +0800

    update 分頁器元件樣式調整

--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -1,5 +1,6 @@
 <template>
   <el-pagination
+    class="pagination"
     :current-page="currentPage"
     :page-size="pageSize"
     :total="total"
@@ -40,4 +41,41 @@
 </script>
 
 <style lang="scss" scoped>
+
+  .pagination {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+
+    :deep(.btn-prev), //上一頁按鈕
+    :deep(.btn-next), // 下一頁
+    :deep(.el-pager li) { //中間頁碼
+      min-width: 32px;
+      height: 32px;
+      margin: 0;
+
+      border-radius: 6px;
+      background-color: transparent;
+
+      color: #141c26;
+      font-size: 16px;
+      font-weight: 500;
+    }
+
+    :deep(.el-pager li.is-active) {
+      background-color: #fec96b;
+      color: #141c26;
+    }
+
+    :deep(.btn-prev:hover),
+    :deep(.btn-next:hover),
+    :deep(.el-pager li:hover) {
+      color: #f29b00;
+    }
+
+    :deep(.el-pager li.is-active:hover) {
+      color: #141c26;
+    }
+  }
+
 </style>
