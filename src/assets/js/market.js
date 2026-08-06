15979bf7b350662782c30c4c1a48f6ac93f1db11
cyling <chuang5517@gmail.com>
2026-08-06 13:14:58 +0800

    update header RWD

--- src/assets/js/market.js
+++ src/assets/js/market.js
@@ -8,17 +8,17 @@ createApp(Footer).mount("#footerApp");
 
 
 // 判斷視窗寬度
-const width = window.innerWidth;
-console.log(width);
-const header = document.getElementById('headerApp');
-const footer = document.getElementById('footerApp');
-const heroBtn = document.getElementById('btn-hero');
-if (width < 768) {
-   // heroBtn.style.display = 'none';
-   header.style.display = 'none';
-   footer.style.display = 'none';
-} else if (width >= 768) {
-   // heroBtn.style.display = 'block';
-   header.style.display = 'block';
-   footer.style.display = 'block';
-}
+// const width = window.innerWidth;
+// console.log(width);
+// const header = document.getElementById('headerApp');
+// const footer = document.getElementById('footerApp');
+// const heroBtn = document.getElementById('btn-hero');
+// if (width < 768) {
+//    // heroBtn.style.display = 'none';
+//    header.style.display = 'none';
+//    footer.style.display = 'none';
+// } else if (width >= 768) {
+//    // heroBtn.style.display = 'block';
+//    header.style.display = 'block';
+//    footer.style.display = 'block';
+// }
