5d6e0449f58e02bbc5a94c95f0fce46d48b0f3e8
s3618222 <s3618222@gmail.com>
2026-08-09 13:48:29 +0800

    update 會員中心側邊導覽列基本rwd

--- src/views/member.vue
+++ src/views/member.vue
@@ -1,59 +1,119 @@
 <template>
-  <div class="member-center">
-    <aside class="member-sidebar">
-      <RouterLink
-        :to="{ name: 'member-battle' }"
-        class="member-sidebar-link"
-      >
-        我的約戰
-      </RouterLink>
+  <Header solid />
+  <div class="member-page">
+    <main class="member-center">
 
-      <RouterLink
-        :to="{ name: 'member-forum' }"
-        class="member-sidebar-link"
-      >
-        我的論壇
-      </RouterLink>
+      <aside class="member-sidebar">
+        <RouterLink
+          :to="{ name: 'member-profile' }"
+          class="member-sidebar-link"
+        >
+          個人資料
+        </RouterLink>
 
-      <RouterLink
-        :to="{ name: 'member-exchange' }"
-        class="member-sidebar-link"
-      >
-        我的交換
-      </RouterLink>
-    </aside>
+        <RouterLink
+          :to="{ name: 'member-battle' }"
+          class="member-sidebar-link"
+        >
+          我的約戰
+        </RouterLink>
 
-    
-    <main class="member-content">
-      <RouterView />
+        <RouterLink
+          :to="{ name: 'member-forum' }"
+          class="member-sidebar-link"
+        >
+          我的論壇
+        </RouterLink>
+
+        <RouterLink
+          :to="{ name: 'member-exchange' }"
+          class="member-sidebar-link"
+        >
+          我的交換
+        </RouterLink>
+
+        <RouterLink
+          :to="{ name: 'member-appeal' }"
+          class="member-sidebar-link"
+        >
+          我的申訴
+        </RouterLink>
+
+        <RouterLink
+          :to="{ name: 'member-violation' }"
+          class="member-sidebar-link"
+        >
+          違規紀錄
+        </RouterLink>
+      </aside>
+
+      
+      <section class="member-content">
+        <RouterView />
+      </section>
+
     </main>
-
   </div>
+  <Footer />
 </template>
 
 <script>
+  import Header from "@/components/Header.vue";
+  import Footer from "@/components/Footer.vue";
+
   export default {
-    name: "Member"
+    name: "Member",
+
+    components: {
+      Header,
+      Footer,
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-.member-center {
-  width: min(1200px, calc(100% - 64px));
-  margin: 120px auto 60px;
+  .member-page {
+    padding-top: 88px;
+  }
 
-  display: grid;
-  grid-template-columns: 240px minmax(0, 1fr);
-  gap: 32px;
+  .member-center {
+    width: min(1200px, calc(100% - 64px));
+    margin: 60px auto 60px;
+    display: grid;
+    grid-template-columns: 200px minmax(0, 1fr);
+    gap: 32px;
+  }
+
+  .member-sidebar {
+    border: 1px solid black;
+    text-align: center;
+
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+  }
+
+  section.member-content {
+    padding: 0px; // *修正被其他頁面汙染的css
+    min-width: 0;
+  }
+
+
+@media (max-width: 1024px) {
+  .member-center {
+    grid-template-columns: 1fr;
+    gap: 24px;
+  }
+
+  .member-sidebar {
+    flex-direction: row;
+    overflow-x: auto;
+    gap: 24px;
+
+    padding: 12px 16px;
+    white-space: nowrap;
+    text-align: left;
+  }
 }
 
-.member-sidebar {
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-}
-
-.member-content {
-  min-width: 0;
-}
 </style>
