785ca61ef879d15ac9fe3979fb071989889178ae
tana-joyy <joyce501416@gmail.com>
2026-08-16 10:45:58 +0800

    fix <a>標籤

--- src/components/forum/forumToolbar.vue
+++ src/components/forum/forumToolbar.vue
@@ -11,10 +11,10 @@
       <i class="fa-solid fa-magnifying-glass"></i>
       <input type="text" placeholder="搜尋討論串...">
     </div>
-    <button type="button" class="btnFill btn-post">
+    <a :href="`${baseUrl}forumForm.html`" type="button" class="btnFill btn-post">
       <i class="fa-regular fa-pen-to-square"></i>
-      <a :href="`${baseUrl}forumForm.html`">我要發文</a>
-    </button>
+      <span>我要發文</span>
+    </a>
 
 
 
@@ -130,7 +130,7 @@
   box-shadow: 0 4px 16px rgba(0, 0, 0, 0.2);
 
   // 關鍵：手機版隱藏文字，只留 Icon
-  a {
+  span {
     display: none;
   }
 
@@ -147,7 +147,7 @@
     box-shadow: none;
 
     // 顯示文字，並與左邊 icon 保持間距
-    a {
+    span {
       display: inline;
       margin-left: 6px;
     }
