0ef02e453946737e06f2948f624855149c3de82a
s3618222 <s3618222@gmail.com>
2026-08-19 11:00:12 +0800

    update 允許可以跨域攜帶php session

--- php/common/cors.php
+++ php/common/cors.php
@@ -10,6 +10,10 @@
 
   if (in_array($origin, $allowed_origins)) {
     header("Access-Control-Allow-Origin: " . $origin);
+
+    // 允許跨來源請求攜帶 Cookie；PHP Session 會依靠 Cookie（通常是 PHPSESSID）辨識使用者
+    header("Access-Control-Allow-Credentials: true");
+
     header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
   }
 ?>
