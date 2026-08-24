bd982ce10bcb5c5921ca7181d3c8dca1bc0c3300
tana-joyy <joyce501416@gmail.com>
2026-08-24 22:44:47 +0800

    update 論壇留言上傳圖片api串接

--- php/forum/addComment.php
+++ php/forum/addComment.php
@@ -21,6 +21,7 @@
 
   $articleId = $_POST["art_id"] ?? null;
   $content = $_POST["content"] ?? null;
+  $pic = null;
 
   // 阻止留言內容空白或是輸入空白鍵的資料寫入
   if($content === null || trim($content) === ""){
@@ -28,12 +29,22 @@
     exit();
   }
 
+  // 圖片上傳選填，$_FILES["image"]["error"] 是 PHP 自動記錄「這次上傳有沒有出錯」的狀態碼，UPLOAD_ERR_OK 是 PHP 內建常數，代表「完全沒問題」
+  if (isset($_FILES["image"]) && $_FILES["image"]["error"] === UPLOAD_ERR_OK) {
+    $fileName = time() . "_" . $_FILES["image"]["name"];
+    $targetPath = "../uploads/messages/" . $fileName;
+
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetPath)) {
+      $pic = "http://localhost:8888/Spinix/php/uploads/messages/" . $fileName;
+    }
+  }
+
   try{
     // 寫入留言
-    $insertSql = "INSERT INTO message (mem_id, art_id, content, create_time, is_show) 
-    VALUES (?, ?, ?, NOW(), 1)";
+    $insertSql = "INSERT INTO message (mem_id, art_id, content, create_time, pic, is_show) 
+    VALUES (?, ?, ?, NOW(), ?, 1)";
     $stmt = $pdo ->prepare($insertSql);
-    $stmt->execute([$memberId, $articleId, $content]);
+    $stmt->execute([$memberId, $articleId, $content, $pic]);
 
     $newMsgId = $pdo->lastInsertId(); //拿到新增那筆的 msg_id
 
